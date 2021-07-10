b99327563992ce840ee503a09b3e1956485fdcda
Philip Saa <cowglow@gmail.com>
2021-07-10 11:47:56 +0200

    Updated log display

--- src/components/LogDisplay.tsx
+++ src/components/LogDisplay.tsx
@@ -1,24 +1,20 @@
-import moment from "moment";
-import React from "react";
-import { useAppState } from "../contexts/AppStateContext";
+import React from 'react';
+import { useAppState } from '../contexts/AppStateContext';
+import { formatLog } from '../lib/format-timestamp/format-timestamp';
 
 interface LogDisplayProps {}
 
 export const LogDisplay: React.FC<LogDisplayProps> = () => {
   const { log } = useAppState();
   return (
-    <>
-      <h1>Log Display</h1>
-      <ol>
-        {log.map((entry, index) => {
-          const timestamp = moment.unix(parseInt(entry));
-          return (
-            <li
-              key={index}
-            >{`h: ${timestamp.hours()} m: ${timestamp.minutes()} s: ${timestamp.seconds()}`}</li>
-          );
-        })}
-      </ol>
-    </>
+    <ol>
+      {log.map((entry, key) => {
+        return (
+          <li key={key}>
+            {formatLog(parseInt(entry.timeIn), parseInt(entry.timeOut))}
+          </li>
+        );
+      })}
+    </ol>
   );
 };
