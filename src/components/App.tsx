c9ca57db7be0b7a841a90fa09959f7275272c248
Philip Saa <cowglow@gmail.com>
2021-07-11 11:46:50 +0200

    Add reset button

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,14 +4,15 @@ import { LogDisplay } from './LogDisplay';
 import { useAppState } from '../contexts/AppStateContext';
 
 export const App = () => {
-  const { actionToggle, timestamp } = useAppState();
+  const { actionToggle, timestamp, resetLog } = useAppState();
   return (
     <div>
-      <header>time-accumulator </header>
+      <header>time-accumulator</header>
       <main>
         <ClockDisplay />
         <LogDisplay />
         <button onClick={actionToggle}>{!timestamp ? 'Start' : 'Stop'}</button>
+        <button onClick={resetLog}>Reset Log</button>
       </main>
     </div>
   );
